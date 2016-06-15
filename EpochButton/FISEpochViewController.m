288f23b375d2c6205abfc3f9c1cbfaca865cb284
colinfwalsh <colinfwalsh@gmail.com>
2016-06-15 17:20:13 -0400

    Epoch Lab

---
+++ EpochButton/FISEpochViewController.m
@@ -0,0 +1,47 @@
+//
+//  FISEpochViewController.m
+//  EpochButton
+//
+//  Created by Colin Walsh on 6/15/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISEpochViewController.h"
+
+@interface FISEpochViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *epochLabel;
+
+@end
+
+@implementation FISEpochViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat)epochMethod {
+    return [[NSDate date] timeIntervalSince1970];
+}
+
+- (IBAction)epochButton:(UIButton *)sender {
+    self.epochLabel.text = [NSString stringWithFormat:@"%f", [self epochMethod]];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
